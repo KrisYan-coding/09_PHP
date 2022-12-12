446b83f83ded148d79ba9e30499ab3fd223cafeb
KrisYan <krisyan3318@gmail.com>
2022-12-12 12:03:54 +0800

    new practice const

---
+++ class_practice/php_02_1212_const.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo 12 + 13;
+        echo '<br>';
+        echo 'abc';
+        echo '<br>';
+        echo __DIR__; // 這個 php file 所在的路徑
+        echo '<br>';
+        echo __FILE__; // 這個 php file 所在的路徑 + 檔名
+        echo '<br>';
+        echo __LINE__;  // 這一行程式碼所在的列數
+        echo '<br>';
+
+        
+
+
+
+        
+    
+    ?>
+</body>
+</html>
