0fff90105a7d087ca631c81591f488ecba303e3a
KrisYan <krisyan3318@gmail.com>
2022-12-17 22:56:39 +0800

    表單

--- address-book-p/add.php
+++ address-book-p/add.php
@@ -1,6 +1,5 @@
-
 <!-- model $ controller -->
-<?php 
+<?php
 require './parts/connect_db.php';
 
 $pageName = 'add';
@@ -13,10 +12,43 @@ $title = '新增資料';
 <?php require './parts/html-head.php'; ?>
 <?php require './parts/html-navbar.php'; ?>
 
-<div class="container">
+<div class="container mt-3">
     <div class="row">
-        <div class="col">
-            <h2>add.html</h2>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">新增資料</h5>
+                    <form name="form1" method="POST">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">name</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">email</label>
+                            <input type="text" class="form-control" id="email" name="email">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="mobile" class="form-label">mobile</label>
+                            <input type="text" class="form-control" id="mobile" name="mobile">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="birthday" class="form-label">birthday</label>
+                            <input type="text" class="form-control" id="birthday" name="birthday">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="address" class="form-label">address</label>
+                            <textarea class="form-control" name="address" id="address" cols="30" rows="3"></textarea>
+                            <div class="form-text"></div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- address-book-p/parts/html-head.php
+++ address-book-p/parts/html-head.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= $title ?></title>
+    <title><?= $title ?? '首頁' ?></title>
 
     <!-- BS CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
