ac216166c9afdb7bcb5e69f8123296d9a65a55ee
KrisYan <krisyan3318@gmail.com>
2022-12-17 17:59:46 +0800

    address - prepare

---
+++ address-book-p/index_.php
@@ -0,0 +1,19 @@
+
+<?php 
+require './parts/connect_db.php'
+
+?>
+
+<?php require './parts/html-head.php'; ?>
+<?php require './parts/html-navbar.php'; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>index_.html</h2>
+        </div>
+    </div>
+</div>
+
+<?php require './parts/html-scripts.php'; ?>
+<?php require './parts/html-foot.php'; ?>
